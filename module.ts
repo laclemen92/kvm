8c6e3bbb08c75226c74ffd5980ac05d67d70c6ed
Luke Clement <laclemen92@gmail.com>
2024-10-09 12:06:20 -0500

    add example of how to use the package

--- module.ts
+++ module.ts
@@ -1,10 +1,62 @@
-export * from "./lib/types.ts";
-// export * from "./lib/kvm.ts";
-// export * from "./lib/model.ts";
+/**
+ * @module
+ *
+ * Provides a way to interact with data in DenoKV.
+ * Define a KVMEntity and then use it to perform CRUD
+ * operations in DenoKv. KVMEntity has a zod schema
+ * in order to validate data being stored!
+ *
+ * @example
+ * ```ts
+ * import { create } from "@laclemen92/kvm";
+ * import { z } from "zod";
+ *
+ * const userSchema = z.object({
+ *    id: z.string(),
+ *    email: z.string().email("Invalid email format"),
+ *    age: z.number(),
+ *    sessionId: z.string(),
+ *  }).strict();
+ * export const userEntity: KVMEntity<typeof userSchema.shape> = {
+ *    primaryKey: [{
+ *      name: "users",
+ *      key: "id",
+ *    }],
+ *    secondaryIndexes: [{
+ *      key: [{
+ *        name: "users_by_email",
+ *        key: "email",
+ *       }],
+ *      valueType: ValueType.KEY,
+ *      valueKey: "id",
+ *    }, {
+ *      key: [{
+ *        name: "users_by_session",
+ *        key: "sessionId",
+ *      }],
+ *      valueType: ValueType.KEY,
+ *      valueKey: "id",
+ *    }],
+ *    schema: userSchema,
+ *    name: "users",
+ *  };
+ * type User = z.infer<typeof userEntity.schema>;
+ *
+ * const kv = await Deno.openKv();
+ *
+ * const user = await create<User>(userEntity, kv, {
+ *    id: "user1",
+ *    email: "test@test.com",
+ *    age: 31,
+ *    sessionId: "123"
+ * });
+ * ```
+ */
+
 export * from "./lib/create.ts";
 export * from "./lib/delete.ts";
 export * from "./lib/find.ts";
 export * from "./lib/update.ts";
+export * from "./lib/types.ts";
 
 export type { ZodObject, ZodRawShape } from "zod";
-
